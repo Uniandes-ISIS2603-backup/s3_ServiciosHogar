908197de4e593a64dd5cd105d5b2c98edcc0752b
María Ocampo <mj.ocampov@uniandes.edu.co>
2018-08-30 13:48:13 -0500

    Prestador y Habilidad: Pruebas postman

--- s3_servicioshogar-api/src/main/java/co/edu/uniandes/csw/servicioshogar/resources/HabilidadResource.java
+++ s3_servicioshogar-api/src/main/java/co/edu/uniandes/csw/servicioshogar/resources/HabilidadResource.java
@@ -5,10 +5,85 @@
  */
 package co.edu.uniandes.csw.servicioshogar.resources;
 
+import co.edu.uniandes.csw.servicioshogar.dtos.HabilidadDTO;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
 /**
  *
  * @author estudiante
  */
+@Path("habilidades")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped  
 public class HabilidadResource {
     
+    private static final Logger LOGGER = Logger.getLogger(PrestadorResource.class.getName());
+    
+    /**
+     * 
+     * @param habilidad
+     * @return 
+     */
+    @POST
+    public HabilidadDTO createPrestador(HabilidadDTO  habilidad)
+    {
+        return habilidad;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    @GET
+    public List<HabilidadDTO> getPrestadores()
+    {
+        return null;
+    }
+    
+    /**
+     * 
+     * @param habilidadId
+     * @return 
+     */
+    @GET
+    @Path("{habilidadesId: \\d+}")
+    public HabilidadDTO getPrestador(@PathParam("habilidadesId") Long habilidadId)
+    {
+        return null;
+    }
+    
+    /**
+     * 
+     * @param habilidadId
+     * @param habilidad
+     * @return 
+     */
+    @PUT
+    @Path("{habilidadesId: \\d+}")
+    public HabilidadDTO updatePrestador(@PathParam("habillidadesId") Long habilidadId, HabilidadDTO habilidad){
+        
+        return habilidad;
+    }
+    
+    /**
+     * 
+     * @param habilidadId 
+     */
+    @DELETE
+    @Path("{habilidadesId: \\d+}")
+    public void deletePrestador(@PathParam("habilidadesId") Long habilidadId)
+    {
+        
+    }
 }
--- s3_servicioshogar-api/src/main/java/co/edu/uniandes/csw/servicioshogar/resources/PrestadorResource.java
+++ s3_servicioshogar-api/src/main/java/co/edu/uniandes/csw/servicioshogar/resources/PrestadorResource.java
@@ -5,10 +5,80 @@
  */
 package co.edu.uniandes.csw.servicioshogar.resources;
 
+import co.edu.uniandes.csw.servicioshogar.dtos.PrestadorDTO;
+import co.edu.uniandes.csw.servicioshogar.dtos.PrestadorDetailDTO;
+import co.edu.uniandes.csw.servicioshogar.entities.PrestadorEntity;
+import java.util.List;
+import java.util.logging.Logger;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
 /**
  *
  * @author estudiante
  */
+@Path("prestadores")
+@Produces("application/json")
+@Consumes("application/json")
+@RequestScoped
 public class PrestadorResource {
+ 
+    private static final Logger LOGGER = Logger.getLogger(PrestadorResource.class.getName());
+    
+    /**
+     * 
+     * @param prestador
+     */
+    @POST
+    public PrestadorDTO createPrestador(PrestadorDTO  prestador)
+    {
+        return prestador;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    @GET
+    public List<PrestadorDTO> getPrestadores()
+    {
+        return null;
+    }
+    
+    @GET
+    @Path("{prestadoresId: \\d+}")
+    public PrestadorDTO getPrestador(@PathParam("prestadoresId") Long prestadorId)
+    {
+        return null;
+    }
+    
+    /**
+     * 
+     * @param prestadoresId
+     * @param prestador
+     * @return 
+     * /prestadores/id/habilidades/id
+     */
+    @PUT
+    @Path("{prestadoresId: \\d+}")
+    public PrestadorDTO updatePrestador(@PathParam("prestadoresId") Long prestadoresId, PrestadorDTO prestador){
+        
+        return prestador;
+    }
+    
+    @DELETE
+    @Path("{prestadoresId: \\d+}")
+    public void deletePrestador(@PathParam("prestadoresId") Long prestadorId)
+    {
+        
+    }
+    
     
 }
