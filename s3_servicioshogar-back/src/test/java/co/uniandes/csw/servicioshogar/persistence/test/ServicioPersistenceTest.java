9a055f1a2fd65eb616b17587f4118cf26ea44c66
Steven Tarazona <ys.tarazona@uniandes.edu.co>
2018-09-06 09:44:45 -0500

    paquete persistence.test

---
+++ s3_servicioshogar-back/src/test/java/co/uniandes/csw/servicioshogar/persistence/test/ServicioPersistenceTest.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.uniandes.csw.servicioshogar.persistence.test;
+
+/**
+ *
+ * @author Steven Tarazona <ys.tarazona@uniandes.edu.co>
+ */
+public class ServicioPersistenceTest {
+    
+}
