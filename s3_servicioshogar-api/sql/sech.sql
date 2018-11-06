9f8b0058f8db8772438d4162d78affefb3c1bb50
Daniela Rocha <d.rocha@uniandes.edu.co>
2018-11-06 09:18:37 -0500

    .

--- s3_servicioshogar-api/sql/sech.sql
+++ s3_servicioshogar-api/sql/sech.sql
@@ -1,47 +1,25 @@
-delete from ClienteEntity;
-insert into ClienteEntity (id, nombre, direccion, correo) values (1, 'Andreana Cornilleau', '874 Glacier Hill Point', 'acornilleau0@economist.com');
-insert into ClienteEntity (id, nombre, direccion, correo) values (2, 'Ardella Standbridge', '72 Buhler Junction', 'astandbridge1@clickbank.net');
-insert into ClienteEntity (id, nombre, direccion, correo) values (3, 'Brynn Livingstone', '8 Gina Lane', 'blivingstone2@cornell.edu');
-insert into ClienteEntity (id, nombre, direccion, correo) values (4, 'Latrina Brea', '4064 Reindahl Circle', 'lbrea3@bluehost.com');
-insert into ClienteEntity (id, nombre, direccion, correo) values (5, 'Francesco Chidgey', '1 Packers Court', 'fchidgey4@cbslocal.com');
-insert into ClienteEntity (id, nombre, direccion, correo) values (6, 'Melantha Berridge', '95003 Southridge Road', 'mberridge5@bizjournals.com');
-insert into ClienteEntity (id, nombre, direccion, correo) values (7, 'Gavin Scarffe', '1173 Cody Parkway', 'gscarffe6@japanpost.jp');
-insert into ClienteEntity (id, nombre, direccion, correo) values (8, 'Zorina Varian', '6229 Kennedy Lane', 'zvarian7@goodreads.com');
-insert into ClienteEntity (id, nombre, direccion, correo) values (9, 'Gordie Marcus', '766 Continental Terrace', 'gmarcus8@photobucket.com');
-insert into ClienteEntity (id, nombre, direccion, correo) values (10, 'Adriaens Holywell', '3 Barnett Point', 'aholywell9@wsj.com');
-insert into ClienteEntity (id, nombre, direccion, correo) values (11, 'Lindsey Durran', '60 Leroy Junction', 'ldurrana@springer.com');
-insert into ClienteEntity (id, nombre, direccion, correo) values (12, 'Zeke Karpeev', '0 Donald Lane', 'zkarpeevb@constantcontact.com');
-insert into ClienteEntity (id, nombre, direccion, correo) values (13, 'Sascha Soall', '2 Arkansas Center', 'ssoallc@spiegel.de');
-insert into ClienteEntity (id, nombre, direccion, correo) values (14, 'Ashien Assad', '86 Eastwood Way', 'aassadd@google.es');
-insert into ClienteEntity (id, nombre, direccion, correo) values (15, 'Iorgos Chasen', '411 Nova Plaza', 'ichasene@bloglovin.com');
-insert into ClienteEntity (id, nombre, direccion, correo) values (16, 'Cecil Djakovic', '781 Randy Junction', 'cdjakovicf@skyrock.com');
-insert into ClienteEntity (id, nombre, direccion, correo) values (17, 'Tamma Struys', '46 Dottie Pass', 'tstruysg@statcounter.com');
-insert into ClienteEntity (id, nombre, direccion, correo) values (18, 'Harli Earnshaw', '3349 Coolidge Street', 'hearnshawh@printfriendly.com');
-insert into ClienteEntity (id, nombre, direccion, correo) values (19, 'Tildi Strangman', '0148 Sundown Alley', 'tstrangmani@washington.edu');
-insert into ClienteEntity (id, nombre, direccion, correo) values (20, 'Iolande Ledamun', '366 Green Street', 'iledamunj@ca.gov');
 
-delete from prestadorentity;
-insert into PRESTADORENTITY (id, nombre, cedula) values (1, 'Tessi Perkins', 642702709);
-insert into PRESTADORENTITY (id, nombre, cedula) values (2, 'Bambi Beswetherick', 591237150);
-insert into PRESTADORENTITY (id, nombre, cedula) values (3, 'Tomasina Shiril', 569160688);
-insert into PRESTADORENTITY (id, nombre, cedula) values (4, 'Adiana Turpin', 997948403);
-insert into PRESTADORENTITY (id, nombre, cedula) values (5, 'Marjory Stent', 35727495);
-insert into PRESTADORENTITY (id, nombre, cedula) values (6, 'Eziechiele Laity', 317414876);
-insert into PRESTADORENTITY (id, nombre, cedula) values (7, 'Jonas Vaudre', 918011116);
-insert into PRESTADORENTITY (id, nombre, cedula) values (8, 'Maxy Bremner', 654835077);
-insert into PRESTADORENTITY (id, nombre, cedula) values (9, 'Nevsa Streight', 966207541);
-insert into PRESTADORENTITY (id, nombre, cedula) values (10, 'Heloise McElvogue', 678621139);
 
-delete from ReferenciaEntity;
-insert into ReferenciaEntity(trayectoria, fechaNacimiento, email, telefono, direccion, formacion) values('nulla facilisi cras non velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit vivamus vel nulla eget eros', '9/9/2018', 'jgrivori0@printfriendly.com', 2029034718, '24669 Mayfield Lane', 'congue diam id ornare imperdiet sapien urna pretium nisl ut volutpat sapien');
-insert into ReferenciaEntity(trayectoria, fechaNacimiento, email, telefono, direccion, formacion) values('lorem ipsum dolor sit amet consectetuer adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien', '2/16/2018', 'rkhalid1@icio.us', 3476993759, '182 Bowman Plaza', 'ultrices erat tortor sollicitudin mi sit amet lobortis sapien sapien non');
-insert into ReferenciaEntity(trayectoria, fechaNacimiento, email, telefono, direccion, formacion) values('id ornare imperdiet sapien urna pretium nisl ut volutpat sapien arcu sed augue aliquam erat volutpat in congue', '12/8/2017', 'tivers2@wikispaces.com', 9957529159, '0981 Westridge Lane', 'etiam vel augue vestibulum rutrum rutrum neque aenean auctor gravida sem');
-insert into ReferenciaEntity(trayectoria, fechaNacimiento, email, telefono, direccion, formacion) values('iaculis justo in hac habitasse platea dictumst etiam faucibus cursus urna ut tellus nulla ut erat', '8/29/2018', 'echurchyard3@adobe.com', 9707607855, '7 Westerfield Drive', 'arcu adipiscing molestie hendrerit at vulputate vitae nisl aenean lectus pellentesque');
-insert into ReferenciaEntity(trayectoria, fechaNacimiento, email, telefono, direccion, formacion) values('aliquam erat volutpat in congue etiam justo etiam pretium iaculis justo in hac habitasse platea dictumst etiam', '1/7/2018', 'aabade5@ameblo.jp', 9503573278, '2697 Red Cloud Parkway', 'fusce congue diam id ornare imperdiet sapien urna pretium nisl ut');
-insert into ReferenciaEntity(trayectoria, fechaNacimiento, email, telefono, direccion, formacion) values('felis sed interdum venenatis turpis enim blandit mi in porttitor pede justo eu', '7/11/2018', 'esmallridge6@posterous.com', 3984346404, '89025 Old Gate Terrace', 'justo aliquam quis turpis eget elit sodales scelerisque mauris sit amet');
-insert into ReferenciaEntity(trayectoria, fechaNacimiento, email, telefono, direccion, formacion) values('amet eleifend pede libero quis orci nullam molestie nibh in lectus pellentesque at nulla suspendisse potenti cras in purus eu', '4/1/2018', 'ddemageard7@privacy.gov.au', 8026171892, '57 1st Center', 'ultrices posuere cubilia curae mauris viverra diam vitae quam suspendisse potenti');
-insert into ReferenciaEntity(trayectoria, fechaNacimiento, email, telefono, direccion, formacion) values('quis orci eget orci vehicula condimentum curabitur in libero ut massa volutpat convallis morbi odio', '7/16/2018', 'dsumpton8@ft.com', 8977595441, '60 Moland Avenue', 'donec semper sapien a libero nam dui proin leo odio porttitor id');
-insert into ReferenciaEntity(trayectoria, fechaNacimiento, email, telefono, direccion, formacion) values('curabitur gravida nisi at nibh in hac habitasse platea dictumst aliquam augue quam sollicitudin', '6/2/2018', 'tliversedge9@eepurl.com', 5596229129, '807 Annamark Terrace', 'blandit nam nulla integer pede justo lacinia eget tincidunt eget tempus vel');
-insert into ReferenciaEntity(trayectoria, fechaNacimiento, email, telefono, direccion, formacion) values('ac nulla sed vel enim sit amet nunc viverra dapibus nulla suscipit ligula in', '1/14/2018', 'ltreleavena@dailymail.co.uk', 7011351864, '65 Monument Junction', 'velit eu est congue elementum in hac habitasse platea dictumst morbi');
-insert into ReferenciaEntity(trayectoria, fechaNacimiento, email, telefono, direccion, formacion) values('ultrices mattis odio donec vitae nisi nam ultrices libero non mattis pulvinar nulla pede ullamcorper', '11/18/2018', 'cfaucettb@spotify.com', 2015216217, '084 Sommers Avenue', 'sed magna at nunc commodo placerat praesent blandit nam nulla integer');
-insert into ReferenciaEntity(trayectoria, fechaNacimiento, email, telefono, direccion, formacion) values('adipiscing elit proin interdum mauris non ligula pellentesque ultrices phasellus id sapien in', '10/4/2017', 'mrheamd@bluehost.com', 5482425306, '68 Porter Place', 'blandit ultrices enim lorem ipsum dolor sit amet consectetuer adipiscing elit');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
